7d533d5da5e41847793f85961b5e9e44855351a2
Beleke Ian <ianseeksbeta@gmail.com>
2025-06-04 08:39:32 +0100

    Update of state managemnt: Moved from useLocation to ContextAPI for better state management and pass of user details

--- frontend/src/components/dashboard/detailLayout/ClientLayout.jsx
+++ frontend/src/components/dashboard/detailLayout/ClientLayout.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { searchProviders } from "../../../services/api";
 import ClientUI from "../../clientUI/ClientUI";
+import { UserDataContext } from "../../../context/userContext";
+import { useContext } from "react";
 
-export default function ClientLayout({userData}) {
+export default function ClientLayout() {
+  const {userData: clientData} = useContext(UserDataContext)
+      const userData =
+        clientData || JSON.parse(sessionStorage.getItem("user"));
       const [query, setQuery] = useState('');
     const [providers, setProviders] = useState([]);
     
--- frontend/src/pages/ClientDashboard.jsx
+++ frontend/src/pages/ClientDashboard.jsx
@@ -1,13 +1,9 @@
 import ClientLayout from "../components/dashboard/detailLayout/ClientLayout";
-import { useLocation } from "react-router";
-export default function ClientDashboard() {
 
-    const location = useLocation();
-      const userData =
-        location.state?.user || JSON.parse(sessionStorage.getItem("user"));
+export default function ClientDashboard() {
     return (
         <>
-        <ClientLayout userData={userData}/>
+        <ClientLayout />
         </>
         
     )
